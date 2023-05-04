0978adfd127ca38442f6b1fa37edcd17ba006fa0
Anshu Sharma <71519327+anshu0x@users.noreply.github.com>
2023-05-04 15:16:42 +0530

    title was blurred earlier fixed

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "prismjs/themes/prism-okaidia.css";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  materialOceanic,
-} from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { materialOceanic } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { sampleCode } from "./gradients";
 const Terminal = (_: any, ref: React.LegacyRef<HTMLDivElement>) => {
   const [title, setTitle] = useState("index.ts");
@@ -16,12 +14,13 @@ const Terminal = (_: any, ref: React.LegacyRef<HTMLDivElement>) => {
           <div className="h-3 w-3 bg-red-500 rounded-full" />
           <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full" />
           <div className="ml-2 h-3 w-3 bg-green-500 rounded-full" />
-          <div className="w-2/3 text-center overflow-scroll h-4 ml-4 scrollbar-hide">
-            <input
-              className="text-center focus-visible:outline-none bg-inherit w-full"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
+          <div className="text-center ml-6 flex justify-center">
+            <p
+              contentEditable
+              className="text-white focus-visible:outline-none bg-inherit w-full"
+            >
+              index.ts
+            </p>
           </div>
         </div>
         <div className="mt-4 flex  focus-visible:outline-none">
